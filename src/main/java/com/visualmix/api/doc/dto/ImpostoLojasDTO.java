19d0b3dcfb9f8fa0a1447448b6ba1b60b57882c9
rloureirodev <raphaelloureirodev@gmail.com>
2024-07-31 15:34:28 -0300

    Criado ImpostoLojasDTO

--- src/main/java/com/visualmix/api/doc/dto/ImpostoLojasDTO.java
+++ src/main/java/com/visualmix/api/doc/dto/ImpostoLojasDTO.java
@@ -11,7 +11,7 @@ import lombok.Data;
 @ApiModel(description = "Informações do usuário")
 @Data
 @Builder
-public class EmbalagemLojaDTO {
+public class ImpostoLojasDTO {
 	
 	@ApiModelProperty(value = " ", example = " ")
 	private double produtoId;
@@ -21,76 +21,57 @@ public class EmbalagemLojaDTO {
 
 	@ApiModelProperty(value = " ", example = " ")
 	private int loja;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int qtdEtiquetaGondola;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int frente;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int empilhamento;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int profundidade;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int qtdeMaxDigitacao;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private double precoVenda;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private double custoUltimaEntradaSemIcms;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private LocalDateTime dataInicioProm;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private LocalDateTime dataFimProm;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private double precoPromocao;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int qtdMaxPromocao;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private LocalDateTime dataInicioClube;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private LocalDateTime dataFimClube;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private double precoClube;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private Float percMaxDesconto;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int precoPromTabela;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private Float percDespesas;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int entrega;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private int tipoAcao;	
-
-	@ApiModelProperty(value = " ", example = " ")
-	private double qtdProduto;
-
-	@ApiModelProperty(value = " ", example = " ")
-	private double precoMontagem;	
-
-	@ApiModelProperty(value = " ", example = " ")
-	private String descricaoAux;	
 	
+	///////////////copia da embalagem loja abaixo//
 	
-	// Verificar onde fica isso
-//	@ApiModelProperty(value = " ", example = " ")
-//	private String codIntegracao;
+	@ApiModelProperty(value = " ", example = " ")
+	private String tipoAliquota;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private Float percImposto;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private String situacaoTributaria;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private Float ipi;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private double impostoTotal;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private int cfopCfe;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private double impostoMunicipal;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private double impostoEstadual;
+
+	@ApiModelProperty(value = " ", example = " ")
+	private double impostoFederal;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private double percArrec;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private String codBenFis;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private double aliquotaReducao;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private double percReducaoBase;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private double picmsEfet;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private int motDescIcms;
+	
+	@ApiModelProperty(value = " ", example = " ")
+	private double adremicmsret;
+
+
 
 }
